f27ed1bff6d34095abb8f5df824d776e5975ff1a
trista41024 <trista41024@gmail.com>
2025-08-30 22:57:36 +0800

    feat: 新增訂單管理功能 1. Index 頁面新增訂單表格 2. 新增 OrdersVM 訂單的 ViewModel 3. Index() 新增取得所有訂單資料 (OrdersVM)

--- YC.SYS/Controllers/HomeController.cs
+++ YC.SYS/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using YC.SYS.Models;
+using static YC.SYS.ViewModels.OrdersVM;
 
 namespace YC.SYS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NorthwindContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NorthwindContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var orders = await GetOrdersAsync();
+            return View(orders);
         }
 
         public IActionResult Privacy()
@@ -27,6 +32,28 @@ namespace YC.SYS.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<List<OrderListViewModel>> GetOrdersAsync()
+        {
+            var ordersDetail = await (from o in _context.Orders
+                                      join c in _context.Customers on o.CustomerID equals c.CustomerID
+                                      join e in _context.Employees on o.EmployeeID equals e.EmployeeID
+                                      join od in _context.OrderDetails on o.OrderID equals od.OrderID into orderDetailsGroup
+                                      select new OrderListViewModel
+                                      {
+                                          OrderID = o.OrderID,
+                                          CustomerName = c.ContactName,
+                                          EmployeeName = e.FirstName + " " + e.LastName,
+                                          OrderDate = o.OrderDate,
+                                          ShippedDate = o.ShippedDate,
+                                          ShipCity = o.ShipCity,
+                                          TotalAmount = orderDetailsGroup
+                                              .Sum(od => od.UnitPrice * od.Quantity * (1 - (decimal)od.Discount))
+                                              + (o.Freight ?? 0)
+                                      }).ToListAsync();
+
+            return ordersDetail;
         }
     }
 }
---
+++ YC.SYS/ViewModels/OrdersVM.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YC.SYS.ViewModels
+{
+    public class OrdersVM
+    {
+        /// <summary>總體訂單列表</summary>
+        public class OrderListViewModel
+        {
+            /// <summary>訂單編號</summary>
+            [Display(Name = "訂單編號")]
+            public int OrderID { get; set; }
+
+            /// <summary>客戶姓名</summary>
+            [Display(Name = "客戶姓名")]
+            public string? CustomerName { get; set; }
+
+            /// <summary>負責員工</summary>
+            [Display(Name = "負責員工")]
+            public string? EmployeeName { get; set; }
+
+            /// <summary>訂購日期</summary>
+            [Display(Name = "訂購日期")]
+            [DataType(DataType.Date)]
+            public DateTime? OrderDate { get; set; }
+
+            /// <summary>出貨日期</summary>
+            [Display(Name = "出貨日期")]
+            [DataType(DataType.Date)]
+            public DateTime? ShippedDate { get; set; }
+
+            /// <summary>送貨城市</summary>
+            [Display(Name = "送貨城市")]
+            public string? ShipCity { get; set; }
+
+            /// <summary>總金額</summary>
+            [Display(Name = "總金額")]
+            public decimal TotalAmount { get; set; }
+
+            /// <summary>訂單狀態</summary>
+            [Display(Name = "訂單狀態")]
+            public string Status => GetOrderStatus();
+
+            private string GetOrderStatus()
+            {
+                if (ShippedDate.HasValue)
+                    return "已出貨";
+                else if (OrderDate.HasValue && OrderDate <= DateTime.Now)
+                    return "處理中";
+                else
+                    return "未處理";
+            }
+        }
+
+    }
+}
